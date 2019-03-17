043d5d4c7738f7709c8eaad7104c465f315cbc6a
goose-swan <goose-swan@users.noreply.github.com>
2019-03-17 20:06:37 +0800

    Create algo-172.cpp

---
+++ algo-172.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+//const int N = 6;
+//const int K = 2;
+
+const int N = 3;
+const int K = 1;
+
+
+int main()
+{
+	//int a[N] = {7, 5, 9, 9, 2, 4};
+	int a[N] = {3, 1, 2};
+
+	int dp[]
+
+
+	return 0;
+}
+
